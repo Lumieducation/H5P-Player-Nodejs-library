bd75912c6692b5ac548a371be807475f25786e2d
JPSchellenberg <jps@Lumi.education>
2018-11-26 04:17:39 +0100

    remove build folder from git

--- build/boot.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var server_1 = require("./server");
-server_1["default"].listen(process.env.PORT, function () {
-    console.log('server listening on port ' + process.env.PORT);
-});
-//# sourceMappingURL=boot.js.map
