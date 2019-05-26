bd135762d1635f09380aa5d2a7cff201819a8a44
Nikolas Martens <Nikolas.M@rtens.org>
2019-05-26 20:18:25 +0200

    [#31] Scrape example from h5p.org

---
+++ examples/scrape-examples.js
@@ -0,0 +1,35 @@
+const request = require('request-promise');
+const { JSDOM } = require("jsdom");
+
+const examples = [];
+
+JSDOM.fromURL('https://h5p.org/content-types-and-applications')
+    .then(dom => {
+        const promises = [];
+
+        const lis = dom.window.document.querySelectorAll("li.views-row");
+        for (let i = 0; i < lis.length; i++) {
+            const li = lis[i];
+            const link = li.getElementsByTagName('a')[1];
+            const url = link.getAttribute('href');
+            const name = link.innerHTML;
+            const page = `https://h5p.org${url}`;
+
+            const example = {
+                library: name,
+                name: 'Example',
+                page: page
+            }
+            examples.push(example);
+
+            promises.push(request(page)
+                .then(content => {
+                    const exportUrlMatch = content.match(new RegExp(/"exportUrl":("[^"]+")/));
+                    const exportUrl = JSON.parse(exportUrlMatch[1]);
+                    example.h5p = exportUrl;
+                }));
+        }
+
+        return Promise.all(promises)
+    })
+    .then(() => console.log(JSON.stringify(examples, null, 2)))
