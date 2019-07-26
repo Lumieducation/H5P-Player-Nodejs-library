53a30ecad40ca7efd9d70a464fd70d8aa271dd20
JPSchellenberg <jps@Lumi.education>
2019-07-27 00:42:43 +0200

    Start server for content-tests

--- test/content.ci.js
+++ test/content.ci.js
@@ -1,40 +1,74 @@
 const puppeteer = require('puppeteer');
 const examples = require('../examples/examples.json');
+const path = require('path');
 const PromiseQueue = require('promise-queue');
+const express = require('express');
+const server = express();
+const H5P = require('../src');
 
 const queue = new PromiseQueue(3, Infinity);
 
-puppeteer.launch({ devtools: true }).then(browser => {
-    examples.forEach((example, index) => {
-        queue
-            .add(
-                () =>
-                    new Promise((resolve, reject) => {
-                        browser.newPage().then(page => {
-                            page.on('pageerror', msg => {
-                                console.log('ERROR', example, msg);
-                                reject(new Error(JSON.stringify(example)));
-                            });
+server.use('/h5p/core', express.static(`${path.resolve('')}/h5p/core`));
+server.get('/examples/:key', (req, res) => {
+    const { key } = req.params;
+    const name = path.basename(examples[key].h5p);
+    const dir = `${path.resolve('')}/examples/contents/${name}`;
 
-                            page.goto(
-                                `http://localhost:8080/examples/${index}`
-                            ).then(() => {
-                                page.close();
-                                resolve();
+    server.use('/h5p/libraries', express.static(dir));
+    server.use(`/h5p/content/${name}`, express.static(`${dir}/content`));
+
+    const libraryLoader = (lib, maj, min) =>
+        new Promise(resolve => {
+            resolve(
+                require(`${path.resolve(
+                    ''
+                )}/examples/contents/${name}/${lib}-${maj}.${min}/library.json`)
+            );
+        });
+
+    const h5pObject = require(`${dir}/h5p.json`);
+    const contentObject = require(`${dir}/content/content.json`);
+    return new H5P(libraryLoader)
+        .render(name, contentObject, h5pObject)
+        .then(h5pPage => res.end(h5pPage))
+        .catch(error => res.status(500).end(error.message));
+});
+
+server.listen(8080, () => {
+    console.log(`server running on http://localhost: ${8080}`);
+    puppeteer.launch({ devtools: true }).then(browser => {
+        examples.forEach((example, index) => {
+            queue
+                .add(
+                    () =>
+                        new Promise((resolve, reject) => {
+                            browser.newPage().then(page => {
+                                page.on('pageerror', msg => {
+                                    console.log('ERROR', example, msg);
+                                    reject(new Error(JSON.stringify(example)));
+                                });
+
+                                page.goto(
+                                    `http://localhost:8080/examples/${index}`
+                                ).then(() => {
+                                    page.close();
+                                    resolve();
+                                });
                             });
-                        });
-                    })
-            )
-            .catch(error => {
-                console.log(error);
-                process.exit(1);
-            });
+                        })
+                )
+                .catch(error => {
+                    console.log(error);
+                    process.exit(1);
+                });
+        });
+
+        const interval = setInterval(() => {
+            if (queue.getPendingLength() === 0) {
+                browser.close();
+                clearInterval(interval);
+                process.exit(0);
+            }
+        }, 500);
     });
-
-    const interval = setInterval(() => {
-        if (queue.getPendingLength() === 0) {
-            browser.close();
-            clearInterval(interval);
-        }
-    }, 500);
 });
